411052421973033d91c088fdccfd49e9f369b2ad
hants <hantsawilliams@gmail.com>
2023-06-28 14:31:20 -0400

    added w3

--- scripts_spacy/old/MAYBE_5_manual_reupdate_wp3.py
+++ scripts_spacy/old/MAYBE_5_manual_reupdate_wp3.py
@@ -35,9 +35,12 @@ starttime = time.time()
 filelist = os.listdir('./s3_bucket/json/')
 filelist = filelist[:10000]
 
+## create empty list for tokenized documents and idf dictionaries
+tokenized_documents = []
+idf_results = []
+
 ## define the function to process a single file
 def process_file(file):
-
     tokenized_doc = []  # Initialize tokenized_doc list
     idf_result = {}  # Initialize idf_result dictionary
     
@@ -64,10 +67,6 @@ def process_file(file):
 
     return tokenized_doc, idf_result
 
-## create empty list for tokenized documents and idf dictionary
-tokenized_documents = []
-idf = {}
-
 ## initialize the progress bar with the total number of files
 progress_bar = tqdm(filelist, desc="Processing files", unit="file")
 
@@ -78,17 +77,12 @@ with futures.ProcessPoolExecutor() as executor:
 
     ## wait for all futures to complete
     for future in futures.as_completed(future_results):
-        tokenized_doc, idf_result = future.result()
-
+        tokenized_doc, idf_result = future
         if tokenized_doc is not None:
             tokenized_documents.append(tokenized_doc)
 
         if idf_result is not None:
-            for term, value in idf_result.items():
-                if term in idf:
-                    idf[term] += value
-                else:
-                    idf[term] = value
+            idf_results.append(idf_result)
 
         # update the progress bar
         progress_bar.set_postfix(completed=f"{progress_bar.n}/{progress_bar.total}")
@@ -96,8 +90,18 @@ with futures.ProcessPoolExecutor() as executor:
 
 progress_bar.close()
 
-## Step 5: Divide IDF values by the number of files to get the average
-idf = {term: value / len(filelist) for term, value in idf.items()}
+## Combine IDF dictionaries and calculate average IDF values
+combined_idf = {}
+for idf_result in idf_results:
+    for term, value in idf_result.items():
+        if term in combined_idf:
+            combined_idf[term] += value
+        else:
+            combined_idf[term] = value
+
+## Calculate average IDF values
+num_files = len(filelist)
+idf = {term: value / num_files for term, value in combined_idf.items()}
 
 ## Print the IDF values
 for term, value in idf.items():
@@ -106,8 +110,6 @@ for term, value in idf.items():
 ## capture end time
 endtime = time.time()
 print("Execution Time:", endtime - starttime, "seconds")
-
-
 
 ################################################################################################
 ################################################################################################
