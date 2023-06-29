d40e3871cf850545cd4aac6625850b54f99cc090
hants <hantsawilliams@gmail.com>
2023-06-29 00:19:19 -0400

    additional files

--- scripts/random/conversions_for_frontend.py
+++ scripts/random/conversions_for_frontend.py
@@ -41,6 +41,18 @@ trials_sources_total = trials_sources.groupby(['source']).sum().reset_index()
 trials_sources_total = trials_sources_total.sort_values(by='count', ascending=False)
 trials_sources_total = trials_sources_total.iloc[:100, :]
 
+#7. Create counts for the number of terms with IDF between 0-3, 3-5, 5-7, 7-9, 10, 11, 12+
+idf['idf'] = idf['idf'].astype(float)
+idf['idf_bin'] = pd.cut(idf['idf'], bins=[0, 3, 5, 7, 9, 10, 11, 12, 100], labels=['0-3', '3-5', '5-7', '7-9', '9-10', '10-11', '11-12', '12+'])
+idf_counts = idf.groupby(['idf_bin']).count().reset_index()
+idf_counts = idf_counts[['idf_bin', 'term']]
+idf_counts_buckets = idf_counts.rename(columns={'term': 'term_counts'})
+
+#8. Get a random assort of 5 terms from each IDF bin
+idf_sample = idf.groupby(['idf_bin']).apply(lambda x: x.sample(5)).reset_index(drop=True)
+idf_sample = idf_sample[['idf_bin', 'term']]
+idf_sample_by_bucket = idf_sample.rename(columns={'term': 'terms'})
+
 
 
 ###### DATA FRAME OUTPUTS AS JSON ######
@@ -52,7 +64,12 @@ trials_sources_total.to_json('./data/frontend/trials_sources_total.json', orient
 source_year.to_json('./data/frontend/source_year.json', orient='records')
 #3. trials_year
 trials_year.to_json('./data/frontend/trials_year.json', orient='records')
-#4. types_year
+#4. types_yeargit 
 types_year.to_json('./data/frontend/types_year.json', orient='records')
+
 #5. idf
 idf.to_json('./data/frontend/idf.json', orient='records')
+#6. idf_counts_buckets
+idf_counts_buckets.to_json('./data/frontend/idf_counts_buckets.json', orient='records')
+#7. idf_sample_by_bucket
+idf_sample_by_bucket.to_json('./data/frontend/idf_sample_by_bucket.json', orient='records')
